8a6dcaab4f09678781d22b7109356cff11012cfa
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-04-20 20:05:25 -0300

    first commit

---
+++ AstroTour/AstroTour/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AstroTour project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AstroTour.settings')
+
+application = get_wsgi_application()
