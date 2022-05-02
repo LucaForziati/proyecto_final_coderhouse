112c1364e5865eb987ddc07db5d17581b369870a
Luca Forziati Gangi <lucaforziati@hotmail.com>
2022-05-02 17:32:31 -0300

    se creo una pagina de inicio

--- AstroTour/Usuario/urls.py
+++ AstroTour/Usuario/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('padre', views.padre_template),
     path('login', views.login_request, name = "Login"),
     path('register', views.register, name = 'Register'),
-    path('logout', LogoutView.as_view(template_name = 'login.html'), name = 'Logout'),
+    path('logout', LogoutView.as_view(template_name = 'inicio.html'), name = 'Logout'),
     path('editar', views.editar_perfil, name = 'Editar_perfil'),
     path('register-super', views.register_superusuario, name = 'Register_super'),
     path('perfil', views.perfil_propio, name = 'Perfil_propio'),
